8cd55207fe26cd564eb9ef4c50d34d0c5f5d574b
Quang Thi <dinhquangthi195@gmail.com>
2019-12-25 15:19:32 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app >
+    <div class="blue darken-1 pt-8 pb-6">
+          <h2 class="text-center white--text font-weight-bold">Todo App VueJS + Firebase</h2>
+    </div>
     <v-container>
       <v-row mt="4">
         <v-col col="12">
-          <h2 class="text-center font-weight-bold mb-12">Todo App VueJS + Firebase</h2>
           <v-row>
             <AddFood />
             <FireStore />
